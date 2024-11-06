037f27751090a0e7d2eb295c99a48d23a18b79e4
Fuad Talukder <webfuadtk@gmail.com>
2024-11-06 15:50:51 +0600

    create login form modal using gsap

---
+++ src/components/Form/LoginForm/LoginForm.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const LoginForm = () => {
+  return (
+    <div>
+      <div className="p-2">
+        <div className="mt-10">
+          <div className="text-[#ABACB0] w-full">
+            <p>Email</p>
+            <input
+              className="mt-2 px-3 py-2 w-full border-2 border-[#F7F7F7] outline-none rounded-md"
+              placeholder="Enter your email"
+            ></input>
+          </div>
+          <div className="text-[#ABACB0] w-full mt-5">
+            <p>Password</p>
+            <input
+              className="mt-2 px-3 py-2 w-full border-2 border-[#F7F7F7] outline-none rounded-md"
+              placeholder="Enter your password"
+            ></input>
+          </div>
+        </div>
+        {/* submit button  */}
+        <div className="mt-3 flex justify-end">
+          <a href="">Forgot your password?</a>
+        </div>
+        <button className="mt-5 w-full bg-[#ffac37] text-white font-semibold py-3 px-7 rounded-md hover:bg-[#ffb958] hover:text-white transition-all duration-700">
+          Sign Up
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,39 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import TopHeader from "./TopHeader";
 import Container from "../Container/Container";
 import { FaFacebook, FaTwitterSquare } from "react-icons/fa";
-import { IoLogoYoutube } from "react-icons/io";
+import { IoLogoYoutube, IoMdClose } from "react-icons/io";
 import { FaLinkedinIn } from "react-icons/fa";
 import Link from "next/link";
+import gsap from "gsap";
+import LoginForm from "../Form/LoginForm/LoginForm";
 
 const Header = () => {
+  useEffect(() => {
+    const loginBtn = document.querySelector("#login");
+    const signupForm = document.querySelector("#loginForm");
+    const closeLoginForm = document.querySelector("#loginForm i");
+    if (loginBtn && signupForm) {
+      const tl = gsap.timeline({ paused: true });
+      tl.to("#loginForm", {
+        opacity: 1,
+        duration: 0.5,
+      });
+      loginBtn.addEventListener("click", function () {
+        tl.play();
+      });
+      if (closeLoginForm) {
+        closeLoginForm.addEventListener("click", function () {
+          tl.reverse();
+        });
+      }
+    }
+  }, []);
+
   return (
     <div>
-      <TopHeader></TopHeader>
+      <TopHeader />
       <div className="bg-[#2A4766]">
         <Container>
           <div className="flex justify-between items-center text-white py-3">
@@ -37,7 +61,7 @@ const Header = () => {
               </ul>
             </div>
             <div>
-              <ul className="flex gap-3 uppercase items-center">
+              <ul className="flex gap-3 items-center">
                 <li>
                   <FaFacebook />
                 </li>
@@ -53,12 +77,39 @@ const Header = () => {
                 </li>
 
                 <li className="ps-5 font-semibold text-sm">
-                  <p>Sign in</p>
+                  <button
+                    className="uppercase hover:text-[#ffac37] transition-all duration-700 ease-in-out"
+                    id="login"
+                  >
+                    Sign in
+                  </button>
+                  {/* login modal start */}
+                  <div
+                    id="loginForm"
+                    className="overflow-hidden absolute right-[360px] opacity-0 bg-white drop-shadow-md w-96 text-black px-5 py-5 mt-3"
+                  >
+                    <div>
+                      <div className="flex justify-between items-center border-b-2 border-[#F7F7F7]">
+                        <h5 className="text-[#2A4766] text-lg font-semibold pb-2">
+                          Welcome to HD LUXURY
+                        </h5>
+                        <i className="text-2xl">
+                          <IoMdClose className="p-1 rounded-md cursor-pointer hover:bg-[#ABACB0]" />
+                        </i>
+                      </div>
+                      <div>
+                        <LoginForm></LoginForm>
+                      </div>
+                    </div>
+                  </div>
+                  {/* login modal end */}
                 </li>
                 <li>
                   <p>
                     /
-                    <span className="ps-3 font-semibold text-sm">register</span>
+                    <span className="ps-3 font-semibold text-sm uppercase">
+                      register
+                    </span>
                   </p>
                 </li>
               </ul>
