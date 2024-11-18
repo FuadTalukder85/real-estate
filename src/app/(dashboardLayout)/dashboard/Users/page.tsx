446f9b1c7f77596bb96cf90e275f37d813846cda
Fuad Talukder <webfuadtk@gmail.com>
2024-11-18 14:56:12 +0600

    fetch user from server

--- src/app/(dashboardLayout)/dashboard/Users/page.tsx
+++ src/app/(dashboardLayout)/dashboard/Users/page.tsx
@@ -1,10 +1,14 @@
+"use client";
 import Image from "next/image";
 import proImg from "../../../../assets/images/profileImg01.jpg";
 import { CiEdit } from "react-icons/ci";
 import { AiOutlineDelete } from "react-icons/ai";
 import { FiEye } from "react-icons/fi";
+import { useGetUserQuery } from "../../../../redux/userApi/UserApi";
 
 const Users = () => {
+  const { data } = useGetUserQuery("");
+  console.log(data);
   return (
     <div className="px-10 p-5 text-[#2A4766]">
       <div className="mt-10">
@@ -26,62 +30,39 @@ const Users = () => {
             </tr>
           </thead>
           <tbody>
-            <tr className="flex justify-between items-center p-2 border-s border-r border-b bg-white px-5">
-              <td className="">1</td>
-              <td className="w-1/6 flex items-center space-x-3">
-                <Image
-                  className="rounded-full"
-                  src={proImg}
-                  alt="proImg"
-                  width={40}
-                ></Image>
-                <span className=" font-medium">Fuad Talukder</span>
-              </td>
-              <td className="w-1/6">snjack1920@gmail.com</td>
-              <td className="w-1/6">30 August 2024 01:48</td>
-              <td className="w-1/6">User</td>
-              <td className="w-1/6">
-                <div className="flex gap-5">
-                  <button className="bg-[#ececec] p-2 rounded-md hover:bg-[#2A4766] hover:text-white transition-all duration-700">
-                    <FiEye />
-                  </button>
-                  <button className="bg-[#ececec] p-2 rounded-md hover:bg-[#2A4766] hover:text-white transition-all duration-700">
-                    <CiEdit />
-                  </button>
-                  <button className="bg-red-400 text-white p-2 rounded-md hover:bg-[#2A4766] hover:text-white transition-all duration-700">
-                    <AiOutlineDelete />
-                  </button>
-                </div>
-              </td>
-            </tr>
-            <tr className="flex justify-between items-center p-2 border-s border-r border-b bg-white px-5">
-              <td className="">1</td>
-              <td className="w-1/6 flex items-center space-x-3">
-                <Image
-                  className="rounded-full"
-                  src={proImg}
-                  alt="proImg"
-                  width={40}
-                ></Image>
-                <span className=" font-medium">Fuad Talukder</span>
-              </td>
-              <td className="w-1/6">snjack1920@gmail.com</td>
-              <td className="w-1/6">30 August 2024 01:48</td>
-              <td className="w-1/6">User</td>
-              <td className="w-1/6">
-                <div className="flex gap-5">
-                  <button className="bg-[#ececec] p-2 rounded-md hover:bg-[#2A4766] hover:text-white transition-all duration-700">
-                    <FiEye />
-                  </button>
-                  <button className="bg-[#ececec] p-2 rounded-md hover:bg-[#2A4766] hover:text-white transition-all duration-700">
-                    <CiEdit />
-                  </button>
-                  <button className="bg-red-400 text-white p-2 rounded-md hover:bg-[#2A4766] hover:text-white transition-all duration-700">
-                    <AiOutlineDelete />
-                  </button>
-                </div>
-              </td>
-            </tr>
+            {data?.map((users, index) => (
+              <tr
+                key={index}
+                className="flex justify-between items-center p-2 border-s border-r border-b bg-white px-5"
+              >
+                <td className="">{index + 1}</td>
+                <td className="w-1/6 flex items-center space-x-3">
+                  <Image
+                    className="rounded-full"
+                    src={proImg}
+                    alt="proImg"
+                    width={40}
+                  ></Image>
+                  <span className=" font-medium">{users.name}</span>
+                </td>
+                <td className="w-1/6">{users.email}</td>
+                <td className="w-1/6">{users.date}</td>
+                <td className="w-1/6">User</td>
+                <td className="w-1/6">
+                  <div className="flex gap-5">
+                    <button className="bg-[#ececec] p-2 rounded-md hover:bg-[#2A4766] hover:text-white transition-all duration-700">
+                      <FiEye />
+                    </button>
+                    <button className="bg-[#ececec] p-2 rounded-md hover:bg-[#2A4766] hover:text-white transition-all duration-700">
+                      <CiEdit />
+                    </button>
+                    <button className="bg-red-400 text-white p-2 rounded-md hover:bg-[#2A4766] hover:text-white transition-all duration-700">
+                      <AiOutlineDelete />
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
